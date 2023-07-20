0c36f0114e2e0f8df008b18dcf754b350068984a
Alex Yatsun <starcrafter4444@gmail.com>
2023-07-20 19:30:20 +0300

    Implement working home page

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from kitchen.models import Cook, DishType, Dish
+
+
+class IndexView(View):
+
+    def get_context_data(self, **kwargs):
+        context = {
+            "cookers": get_user_model().objects.count(),
+            "dish_types": DishType.objects.count(),
+            "dishes": Dish.objects.count(),
+        }
+
+        return context
+
+    def get(self, request):
+        return render(request,
+                      "kitchen/index.html",
+                      context=self.get_context_data())
+
