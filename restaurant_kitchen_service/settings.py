27b7505fe5c3903bd0bb88e5a53335663689b350
Alex Yatsun <starcrafter4444@gmail.com>
2023-07-25 20:00:36 +0300

    Add onreder host to ALLOWED_HOSTS in settings

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -27,9 +27,10 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") != False
+DEBUG = os.getenv("DJANGO_DEBUG", "") != False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1",
+                 "kitchen-mate.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
